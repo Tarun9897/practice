9806e0eb6dc78b7e1fd4dba76248a97dec6e08eb
Tarun Mittal <mittal.tarun15@gmail.com>
2020-10-08 03:30:58 +0530

    second

--- src/aer.java
+++ src/aer.java
@@ -3,4 +3,11 @@ public class aer {
     public static void main(String[] args) {
         System.out.println("Enter a number: ");
     }
+
+    public static void main(String[] args) {
+        enter a code line code
+
+                sjnjnx
+
+    }
 }
